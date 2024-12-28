a2a091ca2d9846f60a7af9353cc1bac748095448
Steriousnull <steriousnull@gmail.com>
2024-12-28 21:55:16 +0530

    mean value practice

--- mean.py
+++ mean.py
@@ -1,15 +1,17 @@
-
-
-
-
-
-
-
-
-def calculate_mean(data):
+"""def calculate_mean(data):
     mean = sum(data)/len(data)
     return mean
 
 data_set = {10,15,20,25,30}
 mean_result = calculate_mean(data_set)
-print("Mean: ", mean_result)
+print("Mean: ", mean_result)"""
+
+# practice
+
+"""def calculate(data):
+    mean = sum(data)/len(data)
+    return mean
+
+data = {1,2,3,4,5}
+result = calculate(data)
+print("mean value is : ",result)"""
