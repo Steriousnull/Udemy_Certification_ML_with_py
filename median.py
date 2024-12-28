ebfa1ca3bdcd8d4d8e6c7944a7d22f72088b1a80
Steriousnull <steriousnull@gmail.com>
2024-12-28 21:45:37 +0530

    median value

---
+++ median.py
@@ -0,0 +1,15 @@
+def calculate_median(data):
+    data.sort()
+    n = len(data)
+    mid = n // 2
+
+    if n%2 == 0:
+        median = (data[mid - 1]+data[mid]) / 2
+
+    else:
+        median = data[mid]
+    return median
+    
+data_set = [10,15,20,25,30,7]
+median_result = calculate_median(data_set)
+print("Median : ",median_result)
