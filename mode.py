19af6eb2fd192ef466f43dfd9ae0d0e265bec87f
Steriousnull <steriousnull@gmail.com>
2024-12-28 22:23:09 +0530

    practiced

--- mode.py
+++ mode.py
@@ -3,3 +3,10 @@ from statistics import mode
 data_set = [10,15,20,25,30,20,15,10]
 mode_result = mode(data_set)
 print("Mode : ",mode_result)
+
+"""from statistics import mode
+data = [1,2,3,4,5,6,7,8]
+result = mode(data)
+print(result)
+"""
+
