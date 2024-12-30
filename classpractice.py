07ffc272f3ebc7e7c4a61ac77f39365c22c05793
Steriousnull <steriousnull@gmail.com>
2024-12-31 01:14:23 +0530

    out of certification

---
+++ classpractice.py
@@ -0,0 +1,30 @@
+class alok:
+    def chandru(self, name, age):
+        self.name = name
+        self.age = age
+
+    def findd(self):
+        print(details.name)
+        print(details.age)
+
+    def razor(self):
+        print("soinal")
+
+details = alok()
+details.chandru("csp",22)
+
+
+
+deb = int(input("enter 1 to see details: "))
+
+if deb == 1:
+    details.findd()
+
+else:
+    details.razor()
+    
+    
+
+
+
+
