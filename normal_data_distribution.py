700f614f177f5e7149a47b98ec58956e06f0e948
Steriousnull <steriousnull@gmail.com>
2024-12-30 23:20:30 +0530

    practiced

--- normal_data_distribution.py
+++ normal_data_distribution.py
@@ -12,3 +12,16 @@ plt.title("Norml Distribution - Generated")
 plt.xlabel('Values')
 plt.ylabel('Probabilit Density')
 plt.show()
+
+"""import numpy as np
+import matplotlib.pyplot as plt
+
+mean=70
+dist=10
+volume=1000
+datas=np.random.normal(loc=mean, scale=dist, size=volume)
+plt.hist(datas, bins=30, edgecolor='black', density=True)
+plt.title("todays day")
+plt.xlabel = "Chandru"
+plt.ylabel = "alok"
+plt.show()"""
